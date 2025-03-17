0792662e3373b9792a566e1963bab45a4ce66838
Jailani Abdul Rahman <jailani.har@googlemail.com>
2025-03-17 09:07:06 +0800

    Show image for web and other platform

--- firebaseapp/lib/pages/gallery.dart
+++ firebaseapp/lib/pages/gallery.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appwrite/appwrite.dart';
+import 'package:flutter/foundation.dart' show kIsWeb, Uint8List;
+import 'dart:io';
 
 class GalleryPage extends StatefulWidget {
   const GalleryPage({super.key});
@@ -11,20 +13,36 @@ class GalleryPage extends StatefulWidget {
 class _GalleryPageState extends State<GalleryPage> {
   late Client client;
   late Storage storage;
+  Uint8List? _imageBytes;
+  String? _imagePath;
 
   @override
   void initState() {
     super.initState();
     client = Client()
       .setEndpoint('http://cloud.appwrite.io/v1')
-      .setProject(''); // Your project ID
+      .setProject('67d76af60020ab7cd9b7'); // Your project ID
     storage = Storage(client);
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gallery'),
+      ),
+      body: Column(
+        children: [
+          kIsWeb ?
+            (_imageBytes != null ? 
+              Image.memory(_imageBytes!, fit: BoxFit.cover, width: 200, height: 200)
+              : Text('No image selected'))
+            :
+            (_imagePath != null ? 
+              Image.file(File(_imagePath!), fit: BoxFit.cover, width: 200, height: 200)
+              : Text('No image selected')),
+        ],
+      ),
     );
   }
 }
